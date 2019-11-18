7e1dc29f1b15c137dad85aa6fe6ac09d5b0e056c
Brenton Unger II <brent.unger@oit.edu>
2019-11-18 15:10:09 -0800

    Created a Master Project (Andriod Xamarin.Forms App).

---
+++ JP_PantryAid/JP_PantryAid/JP_PantryAid/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using JP_PantryAid.Models;
+
+namespace JP_PantryAid.ViewModels
+{
+   public class ItemDetailViewModel : BaseViewModel
+   {
+      public Item Item { get; set; }
+      public ItemDetailViewModel(Item item = null)
+      {
+         Title = item?.Text;
+         Item = item;
+      }
+   }
+}
