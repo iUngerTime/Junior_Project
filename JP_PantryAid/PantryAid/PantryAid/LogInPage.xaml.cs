ed6a268daeaa75c0d1338021328ea28342282789
Brenton Unger II <brenton.s.unger@live.com>
2019-12-02 20:05:50 -0800

    Connected Log In page to database!

--- JP_PantryAid/PantryAid/PantryAid/LogInPage.xaml.cs
+++ JP_PantryAid/PantryAid/PantryAid/LogInPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,50 @@ namespace PantryAid
         }
         private void LogInClick(object sender, EventArgs e)
         {
-            //Open DemoPage.xaml Form as a new stack
-            Navigation.PushModalAsync(new NavigationPage(new DemoPage()));
+            //bool for authenticated or not
+            bool authenticated = AuthenticateUser();
+
+            if (authenticated)
+            {
+                //Open DemoPage.xaml Form as a new stack
+                Navigation.PushModalAsync(new NavigationPage(new DemoPage()));
+            }
+            else
+            {
+                DisplayAlert("ERROR", "That is an invalid email", "Return");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>True if user was authenticated correctly, False if not</returns>
+        private bool AuthenticateUser()
+        {
+            bool auth = false;
+
+            //Run SQL command to get user's email
+            string ConnectionString = "server=aura.cset.oit.edu, 5433; database=iUngerTime; UID=iUngerTime; password=iUngerTime";
+            string query = "SELECT UserID, Email FROM Person WHERE LOWER(Email) = '" + emailBox.Text + "';";
+
+            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlCommand comm = new SqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+
+                SqlDataReader read = comm.ExecuteReader();
+
+                if (read.Read())
+                    auth = true;
+
+                con.Close();
+            }
+            catch (Exception)
+            { }
+
+            return auth;
         }
     }
 }
