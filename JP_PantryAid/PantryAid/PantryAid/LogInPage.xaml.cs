259f578ab39c574dca73904eec4678656327cd59
Brenton Unger II <brenton.s.unger@live.com>
2019-12-02 19:46:00 -0800

    Deleted Main Page, Added LogInPage. Connected To Demo page.

---
+++ JP_PantryAid/PantryAid/PantryAid/LogInPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PantryAid
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LogInPage : ContentPage
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+        private void LogInClick(object sender, EventArgs e)
+        {
+            //Open DemoPage.xaml Form as a new stack
+            Navigation.PushModalAsync(new NavigationPage(new DemoPage()));
+        }
+    }
+}
