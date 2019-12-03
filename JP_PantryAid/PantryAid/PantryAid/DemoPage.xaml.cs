61e4c60b5d0354739b3f35abcd47e53cbc965ce9
ThomasEEEEEEEE <56699831+ThomasEEEEEEEE@users.noreply.github.com>
2019-12-02 17:35:17 -0800

    Added a demopage to demonstrate a connection for our Dec 3rd presentation.

---
+++ JP_PantryAid/PantryAid/PantryAid/DemoPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Data.SqlClient;
+
+namespace PantryAid
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DemoPage : ContentPage
+    {
+        public DemoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void IncrButton_Clicked(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(IncrLabel.Text);
+            ++i;
+            IncrLabel.Text = i.ToString();
+        }
+
+        private void DecrButton_Clicked(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(IncrLabel.Text);
+            --i;
+            IncrLabel.Text = i.ToString();
+        }
+
+        private async void ConButton_Clicked(object sender, EventArgs e)
+        {
+            string ConnectionString = "server=aura.cset.oit.edu, 5433; database=iUngerTime; UID=iUngerTime; password=iUngerTime";
+            string query = "SELECT CommonName FROM INGREDIENT;";
+
+            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlCommand comm = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                
+                SqlDataReader read = comm.ExecuteReader();
+                if (read.Read())
+                    resultLabel.Text = read.GetValue(0).ToString();
+                await DisplayAlert("YEET", "Successfully connected!", "YES");
+
+                con.Close();
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("NOT YEET", "Successfully failed!", "NO");
+            }
+        }
+    }
+}
