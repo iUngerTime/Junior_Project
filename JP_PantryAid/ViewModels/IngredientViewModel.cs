1c4e717361bdd34d3e15175c18d3b375dc5dbb8a
Brenton Unger II <brent.unger@oit.edu>
2019-12-23 22:34:54 -0800

    Created DLL for ViewModels to be used through project lifespan

---
+++ JP_PantryAid/ViewModels/IngredientViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    /// <summary>
+    /// View Model for modeling an ingredient from the database of ingredients
+    /// </summary>
+    public class IngredientViewModel
+    {
+        public int IngredientID { get; set; }
+        public string CommonName { get; set; }
+    }
+}
