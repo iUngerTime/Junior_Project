3f500b9d54aec048c1e4a93166aad8e66dd44896
Brenton Unger II <brent.unger@oit.edu>
2020-01-02 11:08:13 -0800

    Made models public

--- JP_PantryAid/PantryAid.Core/Models/Recipe.cs
+++ JP_PantryAid/PantryAid.Core/Models/Recipe.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PantryAid.Core.Models
 {
-    class Recipe
+    public class Recipe
     {
     }
 }
--- JP_PantryAid/PantryAid.Core/Models/User.cs
+++ JP_PantryAid/PantryAid.Core/Models/User.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PantryAid.Core.Models
 {
-    class User
+    public class User
     {
     }
 }
