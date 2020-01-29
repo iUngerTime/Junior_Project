28150756ed21d893a0e06e995cd64800228e5b2f
Brenton Unger II <brenton.s.unger@live.com>
2020-01-28 17:44:16 -0800

    Added comment blocks so not yelled at by intlesense

--- JP_PantryAid/Database_Helpers/UserData.cs
+++ JP_PantryAid/Database_Helpers/UserData.cs
@@ -8,48 +8,103 @@ using PantryAid.Core.Models;
 
 namespace Database_Helpers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class UserData : iUserDataRepo
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currUser"></param>
+        /// <param name="newAlergy"></param>
+        /// <returns></returns>
         public int AddAlergy(User currUser, Ingredient newAlergy)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currUser"></param>
+        /// <param name="newDisliked"></param>
+        /// <returns></returns>
         public int AddDislikedRecipe(User currUser, Recipe_Short newDisliked)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currUser"></param>
+        /// <param name="newFavorite"></param>
+        /// <returns></returns>
         public int AddFavoriteRecipe(User currUser, Recipe_Short newFavorite)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="newUser"></param>
+        /// <returns></returns>
         public int AddUser(User newUser)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="delUser"></param>
+        /// <returns></returns>
         public int DeleteUser(User delUser)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currentInfo"></param>
+        /// <param name="newInfo"></param>
+        /// <returns></returns>
         public int EditUserInfo(User currentInfo, User newInfo)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currUser"></param>
+        /// <param name="alergy"></param>
+        /// <returns></returns>
         public int RemoveAlergy(User currUser, Ingredient alergy)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currUser"></param>
+        /// <param name="nonDisliked"></param>
+        /// <returns></returns>
         public int RemoveDislikedRecipe(User currUser, Recipe_Short nonDisliked)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currUser"></param>
+        /// <param name="nonFavorite"></param>
+        /// <returns></returns>
         public int RemoveFavoriteRecipe(User currUser, Recipe_Short nonFavorite)
         {
             throw new NotImplementedException();
