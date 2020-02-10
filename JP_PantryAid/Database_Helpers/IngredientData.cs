43a38af101bb90093e9a32847f7c802b74b1e748
Brenton Unger II <brent.unger@oit.edu>
2020-02-10 14:38:34 -0800

    created iIngredientModel and started class implementation

---
+++ JP_PantryAid/Database_Helpers/IngredientData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PantryAid.Core.Interfaces;
+using PantryAid.Core.Models;
+
+namespace Database_Helpers
+{
+    public class IngredientData : iIngredientData
+    {
+        public int AddIngredient(Ingredient newIng)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddIngredientsFromRecipeFull(Recipe_Full recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddIngredientsFromRecipeShort(Recipe_Short recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetIngredient(Ingredient ing)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveIngredient(Ingredient oldIng)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JP_PantryAid/PantryAid.Core/Interfaces/iIngredientData.cs
@@ -0,0 +1,19 @@
+﻿using PantryAid.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PantryAid.Core.Interfaces
+{
+    /// <summary>
+    /// Interface for interacting with User data
+    /// </summary>
+    public interface iIngredientData
+    {
+        int AddIngredient(Ingredient newIng);
+        int RemoveIngredient(Ingredient oldIng);
+        int GetIngredient(Ingredient ing);
+        int AddIngredientsFromRecipeShort(Recipe_Short recipe);
+        int AddIngredientsFromRecipeFull(Recipe_Full recipe);
+    }
+}
