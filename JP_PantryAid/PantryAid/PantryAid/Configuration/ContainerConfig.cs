1d8c133e962ab9aa33108cab470b971fa233199b
Brenton Unger II <brent.unger@oit.edu>
2020-02-19 12:39:58 -0800

    Added NuGet package AutoFac. Almost finished dependency injection

---
+++ JP_PantryAid/PantryAid/PantryAid/Configuration/ContainerConfig.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Database_Helpers;
+using PantryAid.Core.Interfaces;
+using PantryAid.Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PantryAid.Configuration
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<SqlServerDataAccess>().As<iSqlServerDataAccess>();
+            builder.RegisterType<IngredientData>().As<iIngredientData>();
+            builder.RegisterType<UserData>().As<iUserDataRepo>();
+
+            return builder.Build();
+        }
+    }
+}
